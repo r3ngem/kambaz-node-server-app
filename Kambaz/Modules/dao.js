ec34528c401d036d7c575b8d968ea9b452212ea4
r3ngem <paclob.m@northeastern.edu>
2025-12-12 21:26:24 -0500

    modules fix

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,25 +1,27 @@
 import model from "./model.js";
 
 export default function ModulesDao() {
-  const findModulesForCourse = (courseId) =>
-    model.find({ course: courseId });
 
-  const createModule = (courseId, module) =>
-    model.create({ ...module, course: courseId });
+  const findModulesForCourse = async (courseId) => {
+    return await model.find({ course: courseId });
+  };
 
-  const deleteModule = (courseId, moduleId) =>
-    model.deleteOne({ _id: moduleId, course: courseId });
+  const createModule = async (courseId, module) => {
+    return await model.create({ ...module, course: courseId });
+  };
 
-  const updateModule = (courseId, moduleId, updates) =>
-    model.updateOne(
-      { _id: moduleId, course: courseId },
-      { $set: updates }
-    );
+  const deleteModule = async (moduleId) => {
+    return await model.deleteOne({ _id: moduleId });
+  };
+
+  const updateModule = async (moduleId, updates) => {
+    return await model.updateOne({ _id: moduleId }, updates);
+  };
 
   return {
     findModulesForCourse,
     createModule,
     deleteModule,
-    updateModule,
+    updateModule
   };
 }
