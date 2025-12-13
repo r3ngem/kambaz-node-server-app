2d14a1173007eae3278036e21f099e387e2243d4
r3ngem <paclob.m@northeastern.edu>
2025-12-12 23:56:55 -0500

    module fix

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,27 +1,28 @@
-import model from "./model.js";
+import ModuleModel from "./model.js";
+import { v4 as uuidv4 } from "uuid";
 
 export default function ModulesDao() {
-
-  const findModulesForCourse = async (courseId) => {
-    return await model.find({ course: courseId });
-  };
-
-  const createModule = async (courseId, module) => {
-    return await model.create({ ...module, course: courseId });
-  };
-
-  const deleteModule = async (moduleId) => {
-    return await model.deleteOne({ _id: moduleId });
-  };
-
-  const updateModule = async (moduleId, updates) => {
-    return await model.updateOne({ _id: moduleId }, updates);
-  };
-
   return {
-    findModulesForCourse,
-    createModule,
-    deleteModule,
-    updateModule
+    createModule: async (courseId, module) => {
+      const newModule = {
+        _id: uuidv4(),
+        ...module,
+        course: courseId,
+        lessons: module.lessons || []
+      };
+      return await ModuleModel.create(newModule);
+    },
+
+    findModulesForCourse: async (courseId) => {
+      return await ModuleModel.find({ course: courseId });
+    },
+
+    updateModule: async (moduleId, updates) => {
+      return await ModuleModel.findByIdAndUpdate(moduleId, updates, { new: true });
+    },
+
+    deleteModule: async (moduleId) => {
+      return await ModuleModel.findByIdAndDelete(moduleId);
+    }
   };
 }
--- Kambaz/Modules/schema.js
+++ Kambaz/Modules/schema.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
-const schema = new mongoose.Schema(
-  {
-    _id: String,
-    name: String,
-    description: String,
-    course: String,
-    lessons: [{ _id: String, name: String, description: String }]
-  },
-  { collection: "modules" }
+
+const moduleSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, default: "" },
+  course: { type: String, required: true },
+  lessons: [
+    {
+      _id: { type: String, default: () => new mongoose.Types.ObjectId().toString() },
+      name: { type: String, required: true },
+      description: { type: String, default: "" }
+    }
+  ]
+}, { collection: "modules" }
 );
-export default schema;
+
+export default moduleSchema;
