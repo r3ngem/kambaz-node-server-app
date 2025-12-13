3f39c78c1a0698632c204daab6dd2df0a5fcbd58
r3ngem <paclob.m@northeastern.edu>
2025-12-12 21:58:10 -0500

    module add fix

--- Kambaz/Modules/routes.js
+++ Kambaz/Modules/routes.js
@@ -16,22 +16,22 @@ export default function ModulesRoutes(app, db) {
     res.send(newModule);
   }
 
-  const deleteModule = async (req, res) => {
-  const { courseId, moduleId } = req.params;
-  const status = await dao.deleteModule(courseId, moduleId);
-  res.send(status);
-}
-
 const updateModule = async (req, res) => {
-  const { courseId, moduleId } = req.params;
+  const { moduleId } = req.params;
   const moduleUpdates = req.body;
-  const status = await dao.updateModule(courseId, moduleId, moduleUpdates);
-  res.send(status);
-}
+  const status = await dao.updateModule(moduleId, moduleUpdates);
+  res.json(status);
+};
+
+const deleteModule = async (req, res) => {
+  const { moduleId } = req.params;
+  const status = await dao.deleteModule(moduleId);
+  res.json(status);
+};
 
 
-  app.put("/api/courses/:courseId/modules/:moduleId", updateModule);
-  app.delete("/api/courses/:courseId/modules/:moduleId", deleteModule);
+  app.put("/api/modules/:moduleId", updateModule);
+  app.delete("/api/modules/:moduleId", deleteModule);
   app.post("/api/courses/:courseId/modules", createModuleForCourse);
   app.get("/api/courses/:courseId/modules", findModulesForCourse);
 }
